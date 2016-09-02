91843c029026954b3c94801a670ee7e0deebc1c2
shamsheev <svr93@i.ua>
2016-09-02 14:47:50 +0300

    add PART_COUNT constant, change mutability

--- stringify-array-async/js/stringify-array-async.js
+++ stringify-array-async/js/stringify-array-async.js
@@ -11,21 +11,32 @@
     const performance = global.performance;
 
     /**
+     * Count of timer tasks for array processing.
+     * @type {number}
+     */
+    const PART_COUNT = 10;
+
+    /**
      * Private stringifyArrayAsync func implementation.
+     * --mutable--
      * @param {Array} dataArr
      * @param {?(function()|Array)} replacer
-     * @param {{ data: string, blockingTime: Array<number> }} result
+     * @param {{
+     *  data: string,
+     *  blockingTime: Array<number>,
+     *  partLength: number
+     * }} result
      * @return {Promise<{ data: string, blockingTime: Array<number> }>}
      */
     const stringifyArrayAsync = function f(dataArr, replacer, result) {
 
-        const currentValue = dataArr.pop();
-        if (currentValue) {
+        const currentValueArr = dataArr.splice(0, result.partLength);
+        if (currentValueArr.length) {
 
             const startBlockingTime = performance.now();
-            const newData = JSON.stringify(currentValue, replacer);
+            const newData = JSON.stringify(currentValueArr, replacer);
 
-            result.data = `${ result.data },${ newData }`;
+            result.data = `${ result.data },${ newData.slice(1, -1) }`;
             result.blockingTime.push(performance.now() - startBlockingTime);
 
             return new Promise((resolve) => {
@@ -56,10 +67,12 @@
 
             replacer = null;
         }
-        return stringifyArrayAsync(dataArr, replacer, {
+        const partLength = ((dataArr.length / PART_COUNT) | 0) + 1;
+        return stringifyArrayAsync(dataArr.slice(), replacer, {
 
             data: '',
             blockingTime: [],
+            partLength: partLength,
         });
     };
 })(self);
